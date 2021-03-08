3d3eb23af5256fc9a65ff9fbc161d70d0ef4f226
KMatura <matura.k04@htlwienwest.at>
2021-03-09 00:48:59 +0100

    Projektdateien hinzufügen.

---
+++ GemueseLaden/Basket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GemueseLaden
+{
+    //
+    class Basket
+    {
+        public ObservableCollection<Veggi> Veggies = new ObservableCollection<Veggi>();
+        public float TotalPrice
+        {
+            get
+            {
+                float gesamtpreis = 0.0f;
+                foreach (var item in Veggies)
+                {
+                    gesamtpreis += item.Price;
+                }
+                return gesamtpreis;
+            }
+        }
+        
+        public void Add(Veggi veggi)
+        {
+            Veggies.Add(veggi);
+            TotalPriceChanged();
+        }
+        public event TotalPriceChangedEventHandler TotalPriceChanged;
+    }
+}
---
+++ GemueseLaden/Veggi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GemueseLaden
+{
+    class Veggi
+    {
+        public string Name { get; set; }
+        public int Menge { get; set; }
+        public float PPE { get; set; }
+        public string Kategorie { get; set; }
+        public float Price
+        {
+            get
+            {
+                if (Kategorie == "A")
+                {
+                    return PPE;
+                }
+                else if (Kategorie == "B")
+                {
+                    return PPE * 1.05f;
+                }
+                else if (Kategorie == "C")
+                {
+                    return PPE * 1.1f;
+                }
+                else
+                {
+                    return PPE;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"Veggi: {Name} Preis/Einheit: {PPE} Menge: {Menge} Kategorie: {Kategorie}";
+        }
+
+    }
+}
